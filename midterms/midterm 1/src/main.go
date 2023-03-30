f4b812cb50e45e031278a75f467c9f4dd0162ea0
RZhaniya <81968879+RZhaniya@users.noreply.github.com>
2023-03-30 17:34:09 +0600

    Add files via upload

--- midterms/midterm 1/src/main.go
+++ midterms/midterm 1/src/main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -25,26 +26,18 @@ var database *sql.DB
 var toInData toIndexData
 var savUsername string
 
-func ProductsHandle(w http.ResponseWriter, r *http.Request) {
+type SearchData struct {
+	search     bool
+	searchText string
+}
 
+func ProductsHandle(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
-	result, err := database.Query("select * from products")
 	if err != nil {
 		log.Println(err)
 	}
 
-	products := []Product{}
-
-	for result.Next() {
-		var P Product
-		err = result.Scan(&P.Id, &P.Car_name, &P.Details, &P.Price)
-		products = append(products, P)
-		if err != nil {
-			panic(err)
-		}
-
-	}
-
+	products := getProductsByName("")
 	toInData := toIndexData{
 		Products: products,
 		Username: savUsername,
@@ -56,12 +49,6 @@ func ProductsHandle(w http.ResponseWriter, r *http.Request) {
 	if nerr != nil {
 		log.Println(nerr)
 	}
-
-}
-
-type SearchData struct {
-	search     bool
-	searchText string
 }
 
 func SearchPage(w http.ResponseWriter, r *http.Request) {
@@ -72,27 +59,14 @@ func SearchPage(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 
-	name := "%" + r.FormValue("productName") + "%"
-	fmt.Println("name:", name)
-	db, err := sql.Open("mysql", "root:password@/world")
+	name := r.FormValue("productName")
+	products := getProductsByName(name)
 
-	result, err := db.Query("SELECT * FROM products WHERE car_name like ?;", name)
-	products := []Product{}
-	for result.Next() {
-		var P Product
-		err = result.Scan(&P.Id, &P.Car_name, &P.Details, &P.Price)
-		products = append(products, P)
-		if err != nil {
-			panic(err)
-		}
-
-	}
 	toInData := toIndexData{
 		Products: products,
 		Username: savUsername,
 	}
 	tpl.ExecuteTemplate(w, "index.html", toInData)
-
 }
 
 // registerHandler serves form for registring new users
@@ -170,23 +144,131 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("incorrect password")
 
 }
+func getProductsByName(name string) []Product {
+	db, err := sql.Open("mysql", "root:password@/world")
+	if err != nil {
+		log.Println(err)
+	}
+	defer db.Close()
+
+	result, err := db.Query("SELECT * FROM products WHERE car_name LIKE ?;", "%"+name+"%")
+	if err != nil {
+		log.Println(err)
+	}
+
+	products := []Product{}
+	for result.Next() {
+		var p Product
+		err = result.Scan(&p.Id, &p.Car_name, &p.Details, &p.Price)
+		if err != nil {
+			log.Println(err)
+		}
+		products = append(products, p)
+	}
+
+	return products
+}
+func filtredProduct(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tpl.ExecuteTemplate(w, "index.html", nil)
+		return
+	}
+
+	r.ParseForm()
+
+	minPriceStr := r.FormValue("minPrice")
+	minPrice, err := strconv.Atoi(minPriceStr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	maxPriceStr := r.FormValue("maxPrice")
+	maxPrice, err := strconv.Atoi(maxPriceStr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	products, err := getFilteredProducts(database, minPrice, maxPrice)
+	if err != nil {
+		log.Println(err)
+	}
+
+	toInData := toIndexData{
+		Products: products,
+		Username: savUsername,
+	}
+	tpl.ExecuteTemplate(w, "index.html", toInData)
+}
+
+func getFilteredProducts(db *sql.DB, minPrice, maxPrice int) ([]Product, error) {
+	// Prepare the SQL query
+	query := "SELECT id, car_name,details, price FROM products WHERE price >= ? AND price <= ?"
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	// Execute the query
+	rows, err := stmt.Query(minPrice, maxPrice)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Create a slice to hold the results
+	products := []Product{}
+
+	// Iterate over the rows
+	for rows.Next() {
+		var p Product
+		if err := rows.Scan(&p.Id, &p.Car_name, &p.Details, &p.Price); err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
+}
 
 var tpl *template.Template
 
 func main() {
+
 	tpl, _ = template.ParseGlob("templates/*.html")
 
 	db, err := sql.Open("mysql", "root:password@/world")
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	if err != nil {
 		log.Println(err)
 	}
+	defer db.Close()
+
 	database = db
-	http.HandleFunc("/", ProductsHandle)
-	http.HandleFunc("/search", SearchPage)
-	http.HandleFunc("/products", ProductsHandle)
-	http.HandleFunc("/register", registerHandler)
-	http.HandleFunc("/login", loginHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	routes := []struct {
+		path    string
+		handler http.HandlerFunc
+	}{
+		{path: "/", handler: ProductsHandle},
+		{path: "/search", handler: SearchPage},
+		{path: "/products", handler: ProductsHandle},
+		{path: "/register", handler: registerHandler},
+		{path: "/login", handler: loginHandler},
+		{path: "/filtred", handler: filtredProduct},
+	}
+
+	for _, route := range routes {
+		http.HandleFunc(route.path, route.handler)
+	}
+
+	products, err := getFilteredProducts(db, 50, 100)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(products)
+
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8080", nil)
 }
